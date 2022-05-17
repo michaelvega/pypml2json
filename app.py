576032fe4f048dbd8dd51b174ab8cf5d32ade47e
Michael Vega <michaelvega8888@gmail.com>
2022-05-17 01:28:30 -0400

    update app.py

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ def converter_post():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='localhost', port=int(os.environ.get('PORT', 8080)))  # should be 0.0.0.0
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))  # should be 0.0.0.0
